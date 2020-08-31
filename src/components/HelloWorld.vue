4d454bd20936acef03df2358b6b7afb93cb0cab5
Squiddymabob <squiddymabob@gmail.com>
2020-08-31 22:40:28 +0100

    basic home page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,16 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12 display-4">🦑</v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Greetings</h1>
-
-        <p class="subheading font-weight-regular">
-            dsahfisdjhf
-        </p>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Subtitle</h2>
-
-        <v-row justify="center">text</v-row>
+  <v-container fill-height fluid pl-0 pr-0>
+    <v-row align="center" justify="center" style="height:100vh" no-gutters dense>
+      <v-col cols="12" lg="6" md="6">
+        <v-card flat tile class="pa-6">
+          <v-row class="text-center">
+            <v-col cols="12 display-4">🦑</v-col>
+            <v-col>
+              <h1 class="display-2 font-weight-bold mb-3">Greetings</h1>
+              <div class="subheading font-weight-regular">I'm Squiddymabob</div>
+            </v-col>
+          </v-row>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
