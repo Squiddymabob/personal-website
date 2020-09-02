c60d2e2929d1da7abe01ee0f9cfcbfc9cecb6397
Squiddymabob <squiddymabob@gmail.com>
2020-09-02 19:37:30 +0100

    test

--- test/Greetings.spec.js
+++ test/Greetings.spec.js
@@ -9,17 +9,22 @@ describe('Greetings.vue', () => {
   let localVue
   let vuetify
   let wrapper
+  let stubs
 
   beforeEach(() => {
     localVue = createLocalVue() // Because of Vuetify use a localVue instance
     vuetify = new Vuetify()
+    stubs = {
+      NuxtContent: true
+    }
   })
 
   it('renders content', () => {
     wrapper = shallowMount(Greetings, {
       localVue,
-      vuetify
+      vuetify,
+      stubs
     })
-    expect(wrapper.text()).toContain('UI design and improving user experience.')
+    expect(wrapper.text()).toContain('Squiddymabob')
   })
 })
